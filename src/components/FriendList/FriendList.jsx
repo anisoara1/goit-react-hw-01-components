bbf6dc887d82ba451d261e771a71dbfb80e2254b
anisoara1 <122683089+anisoara1@users.noreply.github.com>
2023-08-13 15:43:01 +0300

    upload 2 components

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import css from './FriendList.module.css';
+import FriendListItem from './FriendListItem';
+import PropTypes from 'prop-types';
+
+export default function FriendList({ friends }) {
+  return (
+    <ul>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          isOnline={friend.isOnline}
+          name={friend.name}
+        ></FriendListItem>
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
---
+++ src/components/TransactionItem/TransactionItem.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TransactionItem() {
+  return <div>TransactionItem</div>;
+}
